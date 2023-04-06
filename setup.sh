b953011869516c93546253ba8f5dfece6f49cf2f
Short Cord <short@shortcord.com>
2023-04-06 09:24:59 -0400

    Added plugin support for omz and cleaned up setup abit

--- setup.sh
+++ setup.sh
@@ -5,33 +5,44 @@ git submodule update --init
 
 readonly dotfiles=(./dotfiles/*)
 readonly config_content=(./config/*)
+readonly omz_plugins=(./oh-my-zsh-plugins/*)
+readonly secrets=(./dotfiles/secrets/*.gpg)
 
 for dotfile in "${dotfiles[@]}"; do
-	target="${HOME}/.$(basename ${dotfile})"
+	target="${HOME}/.$(basename "${dotfile}")"
 	
 	if test -e "${target}" || test -h "${target}"; then
 		rm "${target}"
   	fi
 
 	echo "symlinking \"${dotfile}\" to \"${target}\""
-	ln -s "$(realpath ${dotfile})" "${target}"
+	ln -s "$(realpath "${dotfile}")" "${target}"
 done
 
 for config_file in "${config_content[@]}"; do
-  	target="${HOME}/.config/$(basename ${config_file})"
+  	target="${HOME}/.config/$(basename "${config_file}")"
 
   	if test -e "${target}" || test -h "${target}"; then
 	    rm "${target}"
   	fi
 
   	echo "symlinking \"${config_file}\" to \"${target}\""
-	ln -s "$(realpath ${config_file})" "${target}"
+	ln -s "$(realpath "${config_file}")" "${target}"
 done
 
-readonly secrets=(./dotfiles/secrets/*.gpg)
+for omz_plugin in "${omz_plugins[@]}"; do
+  	target="${HOME}/.oh-my-zsh/plugins/$(basename "${omz_plugin}")"
+
+	if test -e "${target}" || test -h "${target}"; then
+		rm "${target}"
+	fi
+
+	echo "symlinking \"${omz_plugin}\" to \"${target}\""
+	ln -s "$(realpath "${omz_plugin}")" "${target}"
+done
 
 for secret in "${secrets[@]}"; do
-	name="$(basename ${secret})"
+	name="$(basename "${secret}")"
 	echo "Decrypting secret ${name}"
 	gpg --output "./dotfiles/secrets/${name%".gpg"}" --decrypt --yes "${secret}"
 done
